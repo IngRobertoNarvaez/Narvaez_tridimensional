be056bb607f7de78050bc72556878efb9a39dffe
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-04 16:25:34 -0500

    Ingreso de datos

--- tridimen.c
+++ tridimen.c
@@ -6,5 +6,13 @@ int main(int argc, char const *argv[])
     int ifila;
     int jcolumna;
     int kprofundi;
+    printf("Ingresar Fila i :");
+    scanf("\n %d", &ifila);
+    printf("Ingresar Columna j :");
+    scanf("\n %d", &jcolumna);
+    printf("Ingresar profundidad k :");
+    scanf("\n %d", &kprofundi);
+    int matriz[kprofundi][ifila][jcolumna];
+    srand(time(NULL));
     return 0;
 }
