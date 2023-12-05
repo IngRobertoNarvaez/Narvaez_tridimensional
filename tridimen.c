d7d8fa86b9891bb60be4effa62e557c319eef281
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-04 20:55:36 -0500

    Se iniciara la matriz de valor 0

--- tridimen.c
+++ tridimen.c
@@ -6,14 +6,25 @@ int main()
     int ifila;
     int jcolumna;
     int kcaras;
-    printf("Ingresar Fila i :");
+    printf("Ingresar Fila  :");
     scanf("\n %d", &ifila);
-    printf("Ingresar Columna j :");
+    printf("Ingresar Columna  :");
     scanf("\n %d", &jcolumna);
-    printf("Ingresar caras k :");
+    printf("Ingresar caras  :");
     scanf("\n %d", &kcaras);
-    int matriz[kcaras][ifila][jcolumna];
     srand(time(NULL));
+    int matriz[ifila][jcolumna][kcaras];
+    // Se iniciara las matrices de valor
+    for (int k = 0; k < kcaras; k++)
+    {
+        for (int i = 0; i < ifila; i++)
+        {
+            for (int j = 0; j < jcolumna; j++)
+            {
+                matriz[k][i][j] = 0;
+            }
+        }
+    }
 
     return 0;
 }
